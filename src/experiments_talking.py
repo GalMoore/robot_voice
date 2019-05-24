6fcb2b0cb480b8d55f409057c0b93647c27072d1
GalMoore <gal@galmoore.co.uk>
2019-05-24 18:23:28 +0300

    kick ass speaking with simple call from state machine

---
+++ src/experiments_talking.py
@@ -0,0 +1,5 @@
+import os
+print("talking now")
+# os.system("python3 /home/gal/catkin_ws/src/robot_voice/src/ohbot_say_function.py %s" %("Hello my new "))
+os.system("python3 /home/gal/catkin_ws/src/robot_voice/src/ohbot_say_function.py %s" %(str(101)))
+                    # os.system("python3 /home/gal/catkin_ws/src/robot_face/src/headturn.py %s" %(str(5)))
---
+++ src/ohbot_say_function.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import sys
+from ohbot import ohbot
+# import os
+import time
+import csv
+
+path_to_csv = "/home/gal/catkin_ws/src/robot_chatbot/chatbot.csv"
+
+# number arrives from sys.argv[i] it is a string
+def get_csv_text(number):
+	with open(path_to_csv) as csvfile:
+	    readCSV = csv.reader(csvfile, delimiter=',')
+	    counter = 0
+	    for row in readCSV:
+
+	    	if number==0:
+	    		return("there is no zero")
+
+	    	# elif number==1 and counter==1:
+	    	# 	return(row[1])
+
+	    	if counter == number-1:
+	    		return(row[1])
+	    	counter = counter + 1
+
+# sentence = get_csv_text(0)
+
+# sys.argv[1] means any number passed here as a string
+sentence = get_csv_text(int(sys.argv[1]))
+print("what is sys.argv? " + str(sys.argv[1]) + " " +  str(type(sys.argv[1])))
+print(sentence)
+# print("saying:", sys.argv[1])
+
+
+# sys.argv[1] is the number of the sentence to say
+# grab the sentence from csv file here: 
+
+
+# say the sentence
+ohbot.say(sentence)
